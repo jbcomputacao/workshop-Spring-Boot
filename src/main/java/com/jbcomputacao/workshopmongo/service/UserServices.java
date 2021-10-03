01770c34829efd38051296389912917682190f49
jbcomputacao <jbcomputacao@gmail.com>
2021-10-03 14:34:39 -0300

    Obtendo um usuario por Id.

--- src/main/java/com/jbcomputacao/workshopmongo/service/UserServices.java
+++ src/main/java/com/jbcomputacao/workshopmongo/service/UserServices.java
@@ -7,18 +7,33 @@ import org.springframework.stereotype.Service;
 
 import com.jbcomputacao.workshopmongo.domain.User;
 import com.jbcomputacao.workshopmongo.repository.UserRepository;
+import com.jbcomputacao.workshopmongo.service.Exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
 	
+
 	@Autowired
 	private UserRepository repo;
 	
 	public List<User>findAll(){
 		return repo.findAll();
+
 		
 		
 		
 	}
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não Encontrado");
+			
+		}
+		return user;
+		
+		
+	}
+	
+	
 
 }
