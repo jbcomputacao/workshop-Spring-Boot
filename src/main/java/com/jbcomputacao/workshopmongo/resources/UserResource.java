aedf8cfeca2a8d7d32afd94f6989f6eb9e2fc306
jbcomputacao <jbcomputacao@gmail.com>
2021-10-04 15:57:38 -0300

    atualização de codigo com DELETE

--- src/main/java/com/jbcomputacao/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jbcomputacao/workshopmongo/resources/UserResource.java
@@ -31,24 +31,27 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDtos);
 
 	}
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		User obj =  service.findById(id);
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
-		
-		
+
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
-		User obj =  service.fromDTO(objDTO);
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
-		
-		
-		
-}
-		
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
 }
